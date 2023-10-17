ffe96664575128c99440e91a9e7b23e2d9e231cd
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-10-17 15:46:51 +0530

    usage of chat components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChatUIProvider, ChatViewComponent, ENV, SpacesUIProvider, lightChatTheme } from '@pushprotocol/uiweb';
+
 
 function App() {
   return (
@@ -18,7 +20,17 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      {/* <SpacesUIProvider spaceUI={new SpacesUI} theme={undefined} children={undefined}>
+        
+      </SpacesUIProvider> */}
+      <ChatUIProvider env={ENV.PROD} theme={lightChatTheme}>
+        <ChatViewComponent
+          chatId="4ac5ab85c9c3d57adbdf2dba79357e56b2f9ef0256befe750d9f93af78d2ca68"
+          limit={10}
+        />
+      </ChatUIProvider>
     </div>
   );
 }
